e9fd5839cf6ff35c39cc567c9b99aecee05ac2d6
ekaterina8aleksa <ekaterina.palad87@gmail.com>
2020-07-26 04:54:56 +0300

    fix App

--- phonebook-redux/src/App.js
+++ phonebook-redux/src/App.js
@@ -1,24 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import ContactForm from "./components/ContactForm";
 import ContactList from "./components/ContactList";
 import Filter from "./components/Filter";
 import "react-toastify/dist/ReactToastify.css";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <ContactForm
-          onSubmit={this.formListener}
-          onAddContact={this.addContact}
-        />
-        <Filter />
+const App = () => (
+  <>
+    <ContactForm />
 
-        <h3>Contacts</h3>
+    <Filter />
 
-        <ContactList />
-      </>
-    );
-  }
-}
+    <h3>Contacts</h3>
+
+    <ContactList />
+  </>
+);
 export default App;
